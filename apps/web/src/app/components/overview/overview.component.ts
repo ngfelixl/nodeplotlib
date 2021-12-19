cf4093b0ae90707ed2d8b5113aa7f4a87eed1420
Felix Lemke <flemke.dev@gmail.com>
2021-12-19 22:24:15 +0100

    refactor(web): remove overview component

--- apps/web/src/app/components/overview/overview.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'npl-overview',
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class OverviewComponent {}
